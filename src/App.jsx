f14172a8d7c091047763a887258b88964313b226
AshrafulPatHan <ashrafulpathan3927@gmail.com>
2025-02-25 18:34:40 +0600

    add react tost sweetalert and dasyUI

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
 import { useState } from 'react'
-import './App.css'
+import { toast } from 'react-toastify';
+import Swal from 'sweetalert2';
 
 function App() {
 
   return (
     <>
       <h2>hello world</h2>
+      <button className=' bg-amber-300 w-36 py-2 rounded-3xl'
+      onClick={()=>{toast("hello")}}>
+        click
+      </button>
+      <button className="btn btn-active btn-secondary"
+      onClick={()=>{Swal.fire("SweetAlert2 is working!");}}>Secondary</button>
     </>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import App from "./App.jsx";
+import { Bounce, ToastContainer } from "react-toastify";
 
 const root = document.getElementById("root");
 
@@ -10,5 +11,18 @@ ReactDOM.createRoot(root).render(
     <Routes>
       <Route path="/" element={<App />} />
     </Routes>
+    <ToastContainer
+    position="top-center"
+    autoClose={5000}
+    hideProgressBar={false}
+    newestOnTop={false}
+    closeOnClick={false}
+    rtl={false}
+    pauseOnFocusLoss
+    draggable
+    pauseOnHover
+    theme="light"
+    transition={Bounce}
+    />
   </BrowserRouter>
 );
