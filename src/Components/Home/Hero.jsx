38e3fb77786802cf3499807eebef13ae65eed972
AshrafulPatHan <ashrafulpathan3927@gmail.com>
2025-02-26 17:53:50 +0600

    chang font derictory

--- src/Components/Home/Hero.jsx
+++ src/Components/Home/Hero.jsx
@@ -1,13 +1,25 @@
-
+import Line from '../../assets/image/Brush.png';
 
 const Hero = () => {
     return (
         <div>
             <div>
                 <div>
-
+                    <h2>Discover the Latest</h2>
+                    <h2>Furniture Trends</h2>
+                    <img src={Line} alt="line" />
+                    <p>Shop the Latest Fashion Items and Stay ahead of the style game</p>
+                    <button>Explore More</button>
                 </div>
-                <div></div>
+                <div>
+                    <div>
+                        <img src="" alt="Furniture photo" />
+                        <img src="" alt="Furniture photo" />
+                    </div>
+                    <div>
+                        <img src="" alt="Furniture photo" />
+                    </div>
+                </div>
             </div>
         </div>
     );
