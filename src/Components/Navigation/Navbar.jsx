e724e09cda6ff5aa7c1e4d74ed124c914c8ad112
AshrafulPatHan <ashrafulpathan3927@gmail.com>
2025-02-26 09:02:49 +0600

    add sections

--- src/Components/Navigation/Navbar.jsx
+++ src/Components/Navigation/Navbar.jsx
@@ -3,7 +3,38 @@ import React from 'react';
 const Navbar = () => {
     return (
         <div>
-            nav
+            <div>
+                <div>
+                    <img src="/logo.svg" alt="logo" />
+                </div>
+                <div>
+                    <div>
+                        <p>
+                            Shop Now
+                        </p>
+                        <p>
+                            Home Furniture
+                        </p>
+                        <p>
+                            All Furniture
+                        </p>
+                        <p>
+                            About Us
+                        </p>
+                        <p className='Inter'>
+                            Contact Us
+                        </p>
+                    </div>
+                    <div>
+                        <button>
+                            Log in
+                        </button>
+                        <button>
+                            Get start
+                        </button>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
