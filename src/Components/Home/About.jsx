64e91fb76f1f8d7c60e70e46e71042833a556e1f
AshrafulPatHan <ashrafulpathan3927@gmail.com>
2025-03-17 16:04:52 +0600

    addd about section

--- src/Components/Home/About.jsx
+++ src/Components/Home/About.jsx
@@ -1,9 +1,47 @@
-import React from 'react';
+import { FaArrowRight } from "react-icons/fa6";
+
 
 const About = () => {
     return (
-        <div>
-            About
+        <div className="flex flex-col items-start mx-[136px]  ">
+            <div>
+                <div className="Sansation">
+                    <h4 className="text-3xl font-bold">Enjoy Our Latest Fashion Trends and Style</h4>
+                </div>
+                <p className="text-2xl font-light">Ship our curated collection of fashion items and stay ahead of the fashion game</p>
+                <div>
+                    <div>
+                        <h4 className="text-2xl font-bold">Quality</h4>
+                        <div className="Roboto">
+                            <p className="text-[#6A6D70] text-[1rem] ">
+                                Our fashion items are crafted with the highest materials and attention to details
+                            </p>
+                        </div>
+                        <img src="" alt="" />
+                    </div>
+                    <div>
+                        <h4 className="text-2xl font-bold">Style</h4>
+                        <div className="Roboto">
+                            <p className="text-[#6A6D70] text-[1rem] ">
+                                Express you unique style with our wide range of trendy items.
+                            </p>
+                        </div>
+                        <img src="" alt="" />
+                    </div>
+                    <div>
+                        <button className="cursor-pointer text-xl flex justify-center items-center border-2 gap-1 w-[180px] 
+                        h-14 before:absolute before:block before:inset-0 before:-z-10 
+                        before:bg-black text-white after:block hover:after:w-full after:w-0 after:hover:left-0 
+                        after:right-0 after:top-0 after:h-full after:-z-10 after:duration-300 after:bg-sky-900 after:absolute 
+                        relative ">
+                            Explore <FaArrowRight />
+                        </button>
+                    </div>
+                </div>
+            </div>
+            <div>
+                <img src="" alt="" />
+            </div>
         </div>
     );
 };
--- src/Components/Home/Fatback.jsx
+++ src/Components/Home/Fatback.jsx
@@ -1,10 +1,27 @@
 import React from 'react';
+import { FaArrowRight } from 'react-icons/fa6';
 
 const Fatback = () => {
     return (
-        <div className='flex flex-col items-center mx-[136px] '>
+        <div className='flex flex-col items-start mx-[136px] mb-28 '>
             <div>
-
+                <div>
+                    <div className='Roboto'>
+                        <p className=''>Furniture</p>
+                    </div>
+                    <div className='Sansation'>
+                        <h4 className='text-3xl font-bold '>Discover the Latest Trends</h4>
+                    </div>
+                    <p className='text-[#6A6D70] text-lg '>Stay updated with our information and engaging blog posts 
+                        about modern Furniture and Fashion on the industry</p>
+                </div>
+                <button className="cursor-pointer text-xl flex justify-center items-center border-2 gap-1 w-[180px] 
+                h-14 before:absolute before:block before:inset-0 before:-z-10 
+                before:bg-black text-white after:block hover:after:w-full after:w-0 after:hover:left-0 
+                after:right-0 after:top-0 after:h-full after:-z-10 after:duration-300 after:bg-sky-900 after:absolute 
+                relative ">
+                    Explore  <FaArrowRight />
+                </button>
             </div>
         </div>
     );
